b3c373e319e3e1d04ca706476edeb2b159d97bef
Nyrlivy <nyrvlivy@gmail.com>
2022-04-06 00:33:34 -0300

    quase

--- Sistemas de Apostas/Apostas.cs
+++ Sistemas de Apostas/Apostas.cs
@@ -19,7 +19,7 @@ namespace Sistemas_de_Apostas
     {
         static List<numeros> listaNumeros = new List<numeros>();
 
-        public static int Loteria, Cartao, x90, x60, x30, x15;
+        public static int Loteria, Cartao;
 
         public void Gerador(int tipoLoteria, int quantCartao) 
         {
@@ -57,77 +57,108 @@ namespace Sistemas_de_Apostas
 
             Console.Write($"\nDeseja adquirir quantos cartões? ");
             int cartao = int.Parse(Console.ReadLine());
-            Console.WriteLine();
 
             while (true)
             {
                 Console.Write("\nQual Loteria deseja comprar? 90, 60, 30 ou 15? (Recomendamos 60 números): ");
                 Loteria = int.Parse(Console.ReadLine());
-                if ((Loteria == 90) || (Loteria == 60) || (Loteria == 30) || (Loteria == 15)) // 90 até 50; 60 até 20; 30 até 14/ 15 até 7
+                if ((Loteria == 90) || (Loteria == 60) || (Loteria == 30) || (Loteria == 15)) // 90 até 50 - 30 acertos; 60 até 20 - 15 acertos; 30 até 14 - 12 acertos/ 15 até 7 - 5 acertos
                 {
                     switch (Loteria)
                     {
                         case 90:
                             while (true)
                             {
-                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 20 e 50!): ");
-                                int x90 = int.Parse(Console.ReadLine());
-                                int escolhas;
-                                for (int i = 0; i < x90; i++)
+                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 30 e 50 números!): ");
+                                int cartela90 = int.Parse(Console.ReadLine());
+                                if ((cartela90 >= 30) && (cartela90 <= 50))
                                 {
-                                    escolhas = int.Parse(Console.ReadLine());
-                                    listaNumeros.Add(new numeros(escolhas));
-                                }
-                                Console.WriteLine();
-                                if ((x90 >= 20) && (x90 <= 50))
-                                {
-                                    apostas.Gerador(x90);
+                                    Console.Write("\nQuais números deseja apostar? Você pode escolher ", cartela90, " números na faixa de 1 a 90!): ");
+                                    int escolhas;
+                                    for (int i = 0; i < cartela90; i++)
+                                    {
+                                        escolhas = int.Parse(Console.ReadLine());
+                                        if ((escolhas >= 1) && (escolhas <= 90))
+                                        {
+                                            listaNumeros.Add(new numeros(escolhas));
+
+                                        }
+                                        else
+                                        {
+                                            Console.Write("Escolha um número entre 1 e 90! ");
+                                        }
+                                    }
+
+                                    apostas.Gerador(cartela90, cartao);
+
                                     break;
                                 }
                                 else
+                                {
                                     Console.Write("Escolha entre a faixa respectiva: ");
+                                }
                             }
                             break;
 
                         case 60:
                             while (true)
                             {
-                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 6 e 20!): ");
-                                int x60 = int.Parse(Console.ReadLine());
-                                int escolhas;
-                                for (int i = 0; i < x60; i++)
+                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 15 e 20 números!): ");
+                                int cartela60 = int.Parse(Console.ReadLine());
+                                if ((cartela60 >= 15) && (cartela60 <= 20))
                                 {
-                                    escolhas = int.Parse(Console.ReadLine());
-                                    listaNumeros.Add(new numeros(escolhas));
-                                }
-                                Console.WriteLine();
-                                if ((x60 >= 6) && (x60 <= 20))
-                                {
-                                    apostas.Gerador(x60);
+                                    Console.Write("\nQuais números deseja apostar? Você pode escolher ", cartela60, " números na faixa de 1 a 60!): ");
+                                    int escolhas;
+                                    for (int i = 0; i < cartela60; i++)
+                                    {
+                                        escolhas = int.Parse(Console.ReadLine());
+                                        if ((escolhas >= 1) && (escolhas <= 60))
+                                        {
+                                            listaNumeros.Add(new numeros(escolhas));
+
+                                        }
+                                        else
+                                        {
+                                            Console.Write("Escolha um número entre 1 e 60! ");
+                                        }
+                                    }
+
+                                    apostas.Gerador(cartela60, cartao);
+
                                     break;
                                 }
                                 else
                                 {
                                     Console.Write("Escolha entre a faixa respectiva: ");
-                                }  
+                                }
                             }
                             break;
 
                         case 30:
                             while (true)
                             {
-                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 3 e 14!): ");
-                                int x30 = int.Parse(Console.ReadLine());
-                                int escolhas;
-                                for (int i = 0; i < x30; i++)
+                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 12 e 14 números!): ");
+                                int cartela30 = int.Parse(Console.ReadLine());
+                                if ((cartela30 >= 12) && (cartela30 <= 14))
                                 {
-                                    escolhas = int.Parse(Console.ReadLine());
-                                    listaNumeros.Add(new numeros(escolhas));
-                                }
-                                Console.WriteLine();
-                                if ((x30 >= 3) && (x30 <= 14))
-                                {
-                                    apostas.Gerador(x30);
+                                    Console.Write("\nQuais números deseja apostar? Você pode escolher ", cartela30, " números na faixa de 1 a 30!): ");
+                                    int escolhas;
+                                    for (int i = 0; i < cartela30; i++)
+                                    {
+                                        escolhas = int.Parse(Console.ReadLine());
+                                        if ((escolhas >= 1) && (escolhas <= 30))
+                                        {
+                                            listaNumeros.Add(new numeros(escolhas));
+
+                                        }
+                                        else
+                                        {
+                                            Console.Write("Escolha um número entre 1 e 30! ");
+                                        }
+                                    }
+
+                                    apostas.Gerador(cartela30, cartao);
+
                                     break;
                                 }
                                 else
@@ -140,29 +171,35 @@ namespace Sistemas_de_Apostas
                         case 15:
                             while (true)
                             {
-                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 1 e 7!): ");
-                                int x15 = int.Parse(Console.ReadLine());
-                                int escolhas;
-                                for (int i = 0; i < x15; i++)
+                                Console.Write("\nQuantos números deseja apostar? Você pode escolher entre 5 e 7!): ");
+                                int cartela15 = int.Parse(Console.ReadLine());
+                                if ((cartela15 >= 5) && (cartela15 <= 7))
                                 {
-                                    escolhas = int.Parse(Console.ReadLine());
-                                    listaNumeros.Add(new numeros(escolhas));
-                                }
-                                Console.WriteLine();
-                                if ((x15 >= 1) && (x15 <= 7))
-                                {
-                                    apostas.Gerador(x15);
-                                    break;
-                                }
-                                else
-                                {
-                                    Console.Write("Escolha entre a faixa respectiva: ");
-                                } 
-                            }
-                            break;
-                    }
+                                    Console.Write("\nQuais números deseja apostar? Você pode escolher ", cartela15, " números na faixa de 1 a 15!): ");
+                                    int escolhas;
+                                    for (int i = 0; i < cartela15; i++)
+                                    {
+                                        escolhas = int.Parse(Console.ReadLine());
+                                        if ((escolhas >= 1) && (escolhas <= 15))
+                                        {
+                                            listaNumeros.Add(new numeros(escolhas));
 
+                                        }
+                                        else
+                                        {
+                                            Console.Write("Escolha um número entre 1 e 15! ");
+                                        }
+                                    }
+
+                                    apostas.Gerador(cartela15, cartao);
+
+                                    
+                                }
+                            }
+                    }
+                            break;                       
                 }
+
                 else
                 {
                     Console.WriteLine();
